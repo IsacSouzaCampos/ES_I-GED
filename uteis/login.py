5a961e7435e1ad92328c55c024166e3f7c8fe808
Isac <isacsouzacampos@hotmail.com>
2020-09-28 19:01:01 -0300

    Aprimorada etapa de login.

--- uteis/login.py
+++ uteis/login.py
@@ -2,6 +2,8 @@ import getpass
 import bcrypt
 from builtins import staticmethod
 
+from reportlab.graphics.barcode import createBarcodeImageInMemory
+
 from uteis import administrador, usuarioComum
 
 
@@ -19,7 +21,7 @@ class LogIn:
                         senha = getpass.getpass('Senha: ').encode()
                         return self.verificar_conta(nome, senha, usuarios)
                     elif opcao == 2:
-                        self.criar_conta()
+                        return self.criar_conta()
                 except Exception as e:
                     return e
 
@@ -43,22 +45,75 @@ class LogIn:
 
         with open('data/.data', 'w') as fout:
             hashed = bcrypt.hashpw(senha, bcrypt.gensalt(rounds=15))
-            print(f'{nome}:{codigo_identificacao}:{hashed.decode()}', file=fout)
+            info_usuario = (nome, codigo_identificacao, hashed.decode())
+            print('usuario_comum:0;administrador:1', file=fout)
+            print(info_usuario, file=fout)
 
         return administrador.Administrador(nome, codigo_identificacao)
 
+    def criar_conta(self):
+        print('[1] Usuario Comum')
+        print('[2] Administrador')
+        opcao = int(input('Opcao: '))
+
+        if opcao == 1:
+            return self.criar_conta_comum()
+        elif opcao == 2:
+            return self.criar_conta_administrador()
+        else:
+            raise Exception('Opcao nao existente')
+
     @staticmethod
-    def criar_conta():
-        pass
+    def criar_conta_comum():
+        nome = str(input('Nome do usuario: '))
+        senha = getpass.getpass('Senha: ').encode()
+
+        with open('data/.data') as fout:
+            text = fout.readlines()
+            for v in text[0].split(';'):
+                if 'usuario_comum' in v:
+                    n_usuarios_comuns = int(v.split(':')[1])+1
+                    codigo_identificacao = 'C' + str(n_usuarios_comuns)
+                else:
+                    n_administradores = int(v.split(':')[1])+1
+            hashed = bcrypt.hashpw(senha, bcrypt.gensalt(rounds=15))
+            text[0] = f'usuario_comum:{n_usuarios_comuns};administrador:{n_administradores}'
+            info_usuario = (nome, codigo_identificacao, hashed.decode())
+            text.append(str(info_usuario))
+            fout.writelines(text)
+        return usuarioComum.UsuarioComum(nome, codigo_identificacao)
+
+    @staticmethod
+    def criar_conta_administrador():
+        nome = str(input('Nome do usuario: '))
+        senha = getpass.getpass('Senha: ').encode()
+
+        with open('data/.data') as fout:
+            text = fout.readlines()
+            for v in text[0].split(';'):
+                if 'usuario_comum' in v:
+                    n_usuarios_comuns = int(v.split(':')[1])+1
+                else:
+                    n_administradores = int(v.split(':')[1])+1
+                    codigo_identificacao = 'A' + str(n_administradores)
+            hashed = bcrypt.hashpw(senha, bcrypt.gensalt(rounds=15))
+            text[0] = f'usuario_comum:{n_usuarios_comuns};administrador:{n_administradores}'
+            info_usuario = (nome, codigo_identificacao, hashed.decode())
+            text.append(str(info_usuario))
+            fout.writelines(text)
+        return administrador.Administrador(nome, codigo_identificacao)
 
     @staticmethod
     def verificar_conta(nome, senha, usuarios):
         for line in usuarios.splitlines():
-            line_vec = line.split(':')
-            if line_vec[0] == nome:
-                if bcrypt.checkpw(senha, line_vec[2].encode()):
-                    if line_vec[1][0] == 'A':
-                        return administrador.Administrador(nome, line_vec[1])
-                    elif line_vec[1][0] == 'C':
-                        return usuarioComum.UsuarioComum(nome, line_vec[1])
+            try:
+                line_vec = line.split(':')
+                if line_vec[0] == nome:
+                    if bcrypt.checkpw(senha, line_vec[2].encode()):
+                        if line_vec[1][0] == 'A':
+                            return administrador.Administrador(nome, line_vec[1])
+                        elif line_vec[1][0] == 'C':
+                            return usuarioComum.UsuarioComum(nome, line_vec[1])
+            except:
+                continue
         raise Exception('Erro ao tentar logar')
